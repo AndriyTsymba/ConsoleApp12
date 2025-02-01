c6884d0d16197d0a9d405017dec7999b11a89421
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-01 11:39:24 +0200

    add task1

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp12
 {
-    internal class Program
+    public interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+    public class Array : IOutput
+    {
+        private int[] _arr;
+
+        public Array(int[] arr)
+        {
+            _arr = arr;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (var item in _arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Console.WriteLine("Елементи масиву:");
+            foreach (var item in _arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            IOutput array = new Array(numbers);
+            array.Show();
+            array.Show("Це масив цілих чисел:");
         }
     }
 }
